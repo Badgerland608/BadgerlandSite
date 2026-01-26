275bbfffbf8defc765290bde9289c4e8103623f7
Badgerland608 <info@badgerlandlaundry.com>
2026-01-26 10:28:29 -0600

    Update Hero.jsx

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -11,7 +11,7 @@ function Hero({ setShowModal }) {
       ></div>
 
       {/* Purple Overlay */}
-      <div className="absolute inset-0 bg-purple-900 bg-opacity-0"></div>
+     
 
       {/* Content */}
       <div className="relative z-10 max-w-3xl mx-auto">
